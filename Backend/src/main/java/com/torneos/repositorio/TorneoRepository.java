afd211798b9e13092c7ae820da79323e242e9a1b
cTellez27 <126088185+cTellez27@users.noreply.github.com>
2026-05-27 20:14:17 -0500

    Delete Backend directory
    Esto aun no va

--- Backend/src/main/java/com/torneos/repositorio/TorneoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.torneos.repositorio;
-
-import com.torneos.modelo.Torneo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TorneoRepository extends JpaRepository<Torneo, Long> {
-    
-}
