2f6f76f181abddf70ac66b46c9192168a60d8d21
CharlyUP-300 <cjhoanmurillo@unipacifico.edu.co>
2026-06-14 23:28:17 -0500

    Cargo de la Estructura Definitiva del proyecto
    se Cambio la Estructura del proyecto, ya que con la estructura anterior no funcionaba el programa, ya sea para hacer pruebas.

---
+++ torneos-app/src/main/java/com/torneos/torneos_app/TorneosAppApplication.java
@@ -0,0 +1,13 @@
+package com.torneos.torneos_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorneosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorneosAppApplication.class, args);
+	}
+
+}
---
+++ torneos-app/src/test/java/com/torneos/torneos_app/TorneosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torneos.torneos_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorneosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
