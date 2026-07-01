8d8e01f1670d3f3bab930c6e0f7555075f331549
cTellez27 <126088185+cTellez27@users.noreply.github.com>
2026-07-01 01:23:26 -0500

    Creacion EquipoService.java
    feat(PBI-4): implementar EquipoService para la gestión de equipos

---
+++ torneos-app/src/main/java/com/torneos/torneos_app/servicio/EquipoService.java
@@ -0,0 +1,15 @@
+package com.torneos.servicio;
+
+import com.torneos.dto.EquipoDTO;
+import java.util.List;
+import java.util.Optional;
+
+public interface EquipoService {
+    
+    List<EquipoDTO> listarEquipos();
+    EquipoDTO guardarEquipo(EquipoDTO equipoDTO);
+    Optional<EquipoDTO> buscarEquipoPorId(Long id);
+    void eliminarEquipo(Long id);
+
+    List<EquipoDTO> obtenerEquiposNoInscritos(Long idTorneo);
+}
