2e076eba05932c46171d7fa61e95b40ba53c9239
TellezDev <126088185+cTellez27@users.noreply.github.com>
2026-07-15 18:34:57 -0500

    feat(controller): agregar controladores de registro público

---
+++ torneos-app/src/main/java/com/torneos/torneos_app/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.torneos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/login")
+    public String mostrarlogin() {
+        return "/login";
+    }
+}
